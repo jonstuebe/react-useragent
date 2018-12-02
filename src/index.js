400584a08a207277358d59d59cdc74f1fb216336
Jon Stuebe <jstuebe@gmail.com>
2018-12-02 11:08:58 -0700

    testing out hook implementation

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import MobileDetect from "mobile-detect";
 
 const getUserAgent = () => {
   const md = new MobileDetect(window.navigator.userAgent);
-  const state = {
-    ua: {
-      mobile: md.mobile(),
-      phone: md.phone(),
-      tablet: md.tablet(),
-      os: md.os(),
-      md
-    }
+  return {
+    mobile: md.mobile(),
+    phone: md.phone(),
+    tablet: md.tablet(),
+    os: md.os(),
+    md
   };
-  return state;
 };
+
+export const useUserAgent = () => {
+  const [ua, setUa] = useState({});
+
+  useEffect(() => {
+    setUa(getUserAgent());
+  });
+
+  return ua;
+}
 
 export const withUserAgent = ComposedComponent =>
   class extends Component {
     constructor(props) {
       super(props);
-      this.state = getUserAgent();
+      this.state = { ua: getUserAgent() };
     }
     render() {
       return <ComposedComponent ua={this.state.ua} {...this.props} />;
@@ -29,7 +36,7 @@ export const withUserAgent = ComposedComponent =>
 export class UserAgent extends Component {
   constructor(props) {
     super(props);
-    this.state = getUserAgent();
+    this.state = { ua: getUserAgent() };
   }
   render() {
     const { children, render } = this.props;
